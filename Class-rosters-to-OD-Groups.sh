ca28f205be33cc672b815958182d154c4d05fc60
PWmercy <pwhite@mercy.edu>
2015-05-29 12:19:12 -0400

    Add more details to comments

--- Class-rosters-to-OD-Groups.sh
+++ Class-rosters-to-OD-Groups.sh
@@ -12,7 +12,7 @@ while	read shortname importID
 do
 if [ $shortname == "class" ]
 	then
-	className=$importID #First line is actually class name
+	className=$importID #First line is actually class name, e.g. CART123MTA
 	else
 	#shortname=`echo "${importFirstName:0:1}${importLastName:0:1}$importID" | tr "[:upper:]" "[:lower:]"`
 	echo "$shortname with student ID $importID is in class $className"
