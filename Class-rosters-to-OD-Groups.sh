7b1d471df580b3719c65a4fb3e7d053778e07c52
PWmercy <pwhite@mercy.edu>
2015-06-26 13:30:43 -0400

    Class roster data

--- Class-rosters-to-OD-Groups.sh
+++ Class-rosters-to-OD-Groups.sh
@@ -11,11 +11,11 @@ while	read shortname importID
 
 do
 if [ $shortname == "class" ]
-	then
-	className=$importID #First line is actually class name, e.g. CART123MTA
+	then #First line is actually class name, e.g. CART123MTA
+	className=$importID
 	else
 	#shortname=`echo "${importFirstName:0:1}${importLastName:0:1}$importID" | tr "[:upper:]" "[:lower:]"`
-	echo "$shortname with student ID $importID is in class $className"
+	echo "$shortname with student ID $importID has been added to class $className"
 	dseditgroup -o edit -u $directoryUsername -P $directoryPassword -n $directoryDomain -a $shortname -t user $className
 	#	echo $importFirstName
 	#	echo $importLastName
