1473c65e8c733c59a1150d94c594879e81630219
PWmercy <pwhite@mercy.edu>
2015-09-04 18:40:46 -0400

    Logic change for dscl "no group" error.
    May be unnecessary as problem seems to be related to format of data
    file (Unix v. DOS).

--- Class-rosters-to-OD-Groups.sh
+++ Class-rosters-to-OD-Groups.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 echo "Started"
+
 directoryDomain="/LDAPv3/miniserv5.digiarts.mercy"
+# if running on Server use:
+# directoryDomain="/LDAPv3/127.0.0.1"
+
 #	Username of a directory administrator
 directoryUsername="dadmin"
 #	Password for the above directory administrator
 directoryPassword="x4l1t0l"
-class="cart110dfa"
 
-while	IFS=$'\t' read shortname skipme
+currentclass=""
 
+while	IFS=$'\t' read shortname class
 do
+
+	if [ "$class" != "$currentclass" ]
+		then
+		 currentclass=$class
+	 fi
+
 	#shortname=`echo "${importFirstName:0:1}${importLastName:0:1}$importID" | tr "[:upper:]" "[:lower:]"`
-	echo "$shortname with student ID $importID has been added to class $class"
+	echo "$shortname with student ID $importID has been added to class $currentclass"
 	#dseditgroup -o read -u $directoryUsername -P $directoryPassword -n $directoryDomain $class
  #	echo "dseditgroup -o edit -u $directoryUsername -P $directoryPassword -n $directoryDomain -a $shortname -t user $class"
-dseditgroup -o edit -u $directoryUsername -P $directoryPassword -n $directoryDomain -a $shortname -t user $class
+# dseditgroup -o edit -u $directoryUsername -P $directoryPassword -n $directoryDomain -a $shortname -t user $currentclass
 	#	echo $importFirstName
 	#	echo $importLastName
 	#	echo $importID
