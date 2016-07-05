2d9d5474359182f99cefa3991f1bc7e37db28d54
PWmercy <PWmercy@users.noreply.github.com>
2016-07-05 14:17:26 -0400

    First attempt to delete Mobile Users
    from https://jamfnation.jamfsoftware.com/discussion.html?id=4502

---
+++ deleteusers.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+userList=`dscl . list /Users UniqueID | awk '$2 > 1000 {print $1}'`
+
+echo "Deleting account and home directory for the following users..."
+echo $userList
+for a in $userList ; do
+        find /Users -type d -maxdepth 1 -mindepth 1 -not -name "*.*" -mtime +21 | grep "$a"
+       if [[ $? == 0 ]]; then
+           dscl . delete /Users/"$a"  #delete the account
+            rm -r /Users/"$a"  #delete the home directory
+       fi
+done
