d47f09ba03b03e005bfbfbed5a765eef025b7ea0
PWmercy <PWmercy@users.noreply.github.com>
2016-07-05 14:18:42 -0400

    Update deleteusers.sh

--- deleteusers.sh
+++ deleteusers.sh
@@ -3,11 +3,13 @@
 userList=`dscl . list /Users UniqueID | awk '$2 > 1000 {print $1}'`
 
 echo "Deleting account and home directory for the following users..."
-echo $userList
+
 for a in $userList ; do
+    if [[ "$(id $a | tr '[:upper:]' '[:lower:]')" =~ "bls students" ]]; then
         find /Users -type d -maxdepth 1 -mindepth 1 -not -name "*.*" -mtime +21 | grep "$a"
-       if [[ $? == 0 ]]; then
-           dscl . delete /Users/"$a"  #delete the account
+        if [[ $? == 0 ]]; then
+            dscl . delete /Users/"$a"  #delete the account
             rm -r /Users/"$a"  #delete the home directory
-       fi
+        fi
+    fi
 done
