62058ccb426d7e6ea2e9c004d25fb8d42da2e36a
PWmercy <pwhite@mercy.edu>
2016-09-21 10:47:00 -0400

    First commit
    ExpanDrive
    To copy license file to each existing user library.

---
+++ Deployment/SetupExpanDrive.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# 20160921 As of today, ExpanDrive requires license in each user Library
+# Two files must be on /Users/admin/Desktop
+
+homeFolderlist=`ls /Users`
+
+for theUser in $homeFolderlist; do
+
+# Check if item in /Users is a directory
+
+if [ $theUser != "Shared" ] && [ $theUser != "Guest" ] && [ $theUser != "qubeproxy" ] && [ $theUser != "root" ] && [ $theUser != ".localized" ] && [ -d "/Users/$theUser" ] ; then
+  echo "$theUser is a candidate"
+      if [ -d "/Users/$theUser/Library/Application Support/ExpanDrive/branding" ]; then
+      echo "ExpanDrive folder already exists"
+    else
+      mkdir -p "/Users/$theUser/Library/Application Support/ExpanDrive/branding"
+      echo "Created /Users/$theUser/Library/Application Support/ExpanDrive/branding."
+    fi
+
+    cp /Users/admin/Desktop/configuration.js "/Users/$theUser/Library/Application Support/ExpanDrive/branding/"
+    cp /Users/admin/Desktop/ExpanDrive5.ExpanDriveLicense "/Users/$theUser/Library/Application Support/ExpanDrive/"
+    chmod -R 755 "/Users/$theUser/Library/Application Support/ExpanDrive"
+    chown -R $theUser:staff "/Users/$theUser/Library/Application Support/ExpanDrive"
+    echo "Copied and set permissions for /Users/$theUser/Library/Application Support/ExpanDrive."
+fi
+done
