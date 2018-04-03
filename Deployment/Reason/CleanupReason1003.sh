46e8ba88cc67e7e33040fb0cbd43d2f5fa7d33b6
PWmercy <PWmercy@users.noreply.github.com>
2018-04-03 11:48:33 -0400

    Updates

---
+++ Deployment/Reason/CleanupReason1003.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#################################################
+### Reason 10.3 update doesn't seem to move the Orkester and Factory refills into
+### Application Support as with previous first-runs.
+### This script does some cleanup by moving Orkester to
+### /Library/Application Support/Propellerhead Software/Soundbanks/Orkester/
+### and Factory Sound Bank into
+### /Library/Application Support/Propellerhead Software/Soundbanks/Reason 10/,
+### replacing older versions.
+###
+### We also delete old Reason 9 folder from Soundbanks
+#################################################
+
+ReasonAppPath="/Applications/Reason 10"
+ReasonSoundbankPath="/Library/Application Support/Propellerhead Software/Soundbanks"
+
+echo "moving $ReasonAppPath/Orkester.rfl to $ReasonSoundbankPath/Orkester/"
+mv "$ReasonAppPath/Orkester.rfl" "$ReasonSoundbankPath/Orkester/"
+mv "$ReasonAppPath/Factory Sound Bank.rfl" "$ReasonSoundbankPath/Reason 10/"
+rm -R "$ReasonSoundbankPath/Reason 9/"
