4d0c1c91eca2b5b75d8f11e25d779d4dee260bb4
Phil White <PWmercy@users.noreply.github.com>
2019-03-14 21:34:10 -0400

    Update SetAppleSUS.sh

--- Deployment/SetAppleSUS.sh
+++ Deployment/SetAppleSUS.sh
@@ -2,4 +2,6 @@
 
 # As of 201809
 # Through High Sierra
+
+# 201903 needs update for REPOSADO
 /usr/sbin/softwareupdate --set-catalog http://swupdate2.digiarts.mercy:8088/index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog
