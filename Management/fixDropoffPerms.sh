b46ee34e1266423cdafe391cf0998232bbd41d76
Phil White <pwhite@mercy.edu>
2019-04-30 16:21:27 -0400

    Update fixDropoffPerms.sh
    Work in progress for script to run on all Drop-off-here folders

--- Management/fixDropoffPerms.sh
+++ Management/fixDropoffPerms.sh
@@ -1,11 +1,18 @@
 
 #!/bin/bash
 IFS=$'\n'
-for folder in `ls .`; do
-  echo $folder
-  if [[ "$folder" =~ "Drop-off" ]]; then
-    echo "found one"
-    chmod -R a+rx $folder
-    chmod o-r+w $folder
-  fi
+cd 'FakeShare'
+pwd
+for classFolder in `ls -d *`; do
+    # pwd
+    for innerFolder in `ls -d $classFolder/*`; do
+      echo $innerFolder
+      if [[ "$classFolder/$innerFolder" =~ "Drop-off" ]]; then
+        echo "found one"
+        chmod -R a+rx $classFolder/$innerFolder
+        chmod o-r+w $classFolder/$innerFolder
+      fi
+    done
+    # cd ..
+  pwd
 done
