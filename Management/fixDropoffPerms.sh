4df51ea6f9b2c92861eed425efb02da84fe61dcb
Phil White <pwhite@mercy.edu>
2019-05-01 13:16:59 -0400

    Fixes after test on NAS

--- Management/fixDropoffPerms.sh
+++ Management/fixDropoffPerms.sh
@@ -2,43 +2,35 @@
 
 # 20190430
 # Folders and files dropped off in "Drop-off-here" folders often
-# have bad permissions. This script (intended for scheduled run on Synology NAS)
-# goes through each class folder, finds Drop-off-here, adds rw to everthing, then
+# have bad permissions. This script goes through each class folder,
+# finds # Drop-off-here, adds rw to everthing, then
 # fixes write-only at top level.
-
-# TO DO: correct share name, test on NAS
 
 IFS=$'\n'
 # CART
 cd '/volume1/CART-Class-folders'
-pwd
+
 for classFolder in `ls -d *`; do
-    # pwd
     for innerFolder in `ls -d $classFolder/*`; do
-#      echo $innerFolder
-      if [[ "$classFolder/$innerFolder" =~ "Drop-off-here" ]]; then
-        echo "found one at $classFolder/$innerFolder"
+      if [[ $innerFolder =~ "Drop-off-here" ]]; then
+        echo "found one at $innerFolder"
 #        chmod -R a+rx $classFolder/$innerFolder
 #        chmod o-r+w $classFolder/$innerFolder
+        touch $innerFolder/foundit.txt
       fi
     done
-    # cd ..
-  pwd
 done
 
 # MTEC
 cd '/volume1/MTEC-Class-folders'
-pwd
+
 for classFolder in `ls -d *`; do
-    # pwd
     for innerFolder in `ls -d $classFolder/*`; do
-#      echo $innerFolder
-      if [[ "$classFolder/$innerFolder" =~ "Drop-off-here" ]]; then
-        echo "found one at $classFolder/$innerFolder"
+      if [[ $innerFolder =~ "Drop-off-here" ]]; then
+        echo "found one at $innerFolder"
 #        chmod -R a+rx $classFolder/$innerFolder
 #        chmod o-r+w $classFolder/$innerFolder
+        touch $innerFolder/foundit.txt
       fi
     done
-    # cd ..
-  pwd
 done
