d59e2a67958a5e455fd813953c09c99defe98cf7
Phil White <pwhite@mercy.edu>
2019-06-19 14:04:32 -0400

    Added comments

--- Management/deleteusers.sh
+++ Management/deleteusers.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 
-#https://www.jamf.com/jamf-nation/discussions/4502/remove-old-mobile-accounts#responseChild21985
-
+# https://www.jamf.com/jamf-nation/discussions/4502/remove-old-mobile-accounts#responseChild21985
+#
+# Description below from above link
+#
+# Script builds a list of users with UniqueID's higher than 1000
+# (this way it's only looking at cached AD users). It then checks for home directories older than
+# [mtime] days and then removes the account and home directory for that user. It also only deletes users
+# who are members of our students group in AD. [Experimentation found students group is lower case "student\domain users"]
 
 userList=`dscl . list /Users UniqueID | awk '$2 > 1000 {print $1}'`
 
