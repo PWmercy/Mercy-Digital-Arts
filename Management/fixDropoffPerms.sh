42e18524bf0881ff97d10da5a8bf54539d371818
Phil White <pwhite@mercy.edu>
2019-12-10 11:36:57 -0500

    Sync script versions
    Conform versions on Github with actual script running on NAS1

--- Management/fixDropoffPerms.sh
+++ Management/fixDropoffPerms.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
 
-# 20190913
+# 20191210
+
+# This script has been added to Task Scheduler on CATA-NAS1
 
 # Folders and files dropped off in "Drop-off-here" folders often
-# have bad permissions for instructor. This script goes through each class folder,
-# finds Drop-off-here, adds rw to everything, then
-# fixes write-only at top level.
+# have bad permissions for instructor. This script goes through
+# each class folder, finds Drop-off-here, adds rw to everything,
+# then fixes write-only at top level.
+
 
 IFS=$'\n'
 # CART
@@ -15,8 +17,8 @@ for classFolder in `ls -d *`; do
     for innerFolder in `ls -d $classFolder/*`;  do
       if [[ $innerFolder =~ "Drop-off-here" ]]; then
       # echo "found one at $innerFolder"
-       chmod -R a+rx $classFolder/$innerFolder
-       chmod o-r+w $classFolder/$innerFolder
+       chmod -R a+rx $innerFolder
+       chmod o-r+w $innerFolder
         # touch $innerFolder/foundit.txt
       fi
     done
@@ -29,8 +31,8 @@ for classFolder in `ls -d *`; do
     for innerFolder in `ls -d $classFolder/*`; do
       if [[ $innerFolder =~ "Drop-off-here" ]]; then
       # echo "found one at $innerFolder"
-       chmod -R a+rx $classFolder/$innerFolder
-       chmod o-r+w $classFolder/$innerFolder
+       chmod -R a+rx $innerFolder
+       chmod o-r+w $innerFolder
         # touch $innerFolder/foundit.txt
       fi
     done
