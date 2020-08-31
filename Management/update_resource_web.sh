fce7a036cccbbd333c71e2871f832c0bc4605a5e
Phil White <pwhite@mercy.edu>
2020-08-31 12:46:56 -0400

    Various updates

--- Management/update_resource_web.sh
+++ Management/update_resource_web.sh
@@ -6,10 +6,12 @@ web_folder="/Volumes/Macintosh HD/Development/GitHub/TestRepo/Testweb"
 cd "$docs_folder" || exit
 
 /usr/local/bin/asciidoctor StudentandFacultyresources.adoc
+/usr/local/bin/asciidoctor
 mv StudentandFacultyresources.html "$web_folder"/index.html
+mv labs.html "$web_folder"/labs.html
 
 cd "$web_folder" || exit
 
-/usr/local/bin/git add "index.html"
-/usr/local/bin/git commit -m "Update index.html"
+/usr/local/bin/git add -u
+/usr/local/bin/git commit -m "Update resource pages"
 /usr/local/bin/git push
