7e9ce7955120fc6e75f302b1abade14098367bba
Phil White <pwhite@mercy.edu>
2020-09-23 16:47:53 -0400

    Catch up using atom Git

--- Management/update_resource_web.sh
+++ Management/update_resource_web.sh
@@ -7,12 +7,15 @@ cd "$docs_folder" || exit
 
 /usr/local/bin/asciidoctor StudentandFacultyresources.adoc
 /usr/local/bin/asciidoctor labs.adoc
+/usr/local/bin/asciidoctor wacom.adoc
+
 mv StudentandFacultyresources.html "$web_folder"/index.html
 mv labs.html "$web_folder"/labs.html
+mv wacom.html "$web_folder"/wacom.html
 
 cd "$web_folder" || exit
 
 /usr/local/bin/git add -u
-/usr/local/bin/git commit -m "Update resource pages" && /usr/local/bin/git push
+/usr/local/bin/git commit -m "Update resource pages" && /usr/local/bin/git push --all
 
 exit 0
