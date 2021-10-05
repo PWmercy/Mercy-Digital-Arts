adf867779f0e7dfed10958e5b546fdbca12587f5
PJ White <pwhite@mercy.edu>
2021-10-05 16:29:23 -0400

    Update friendly_comp_name.sh

--- Deployment/friendly_comp_name.sh
+++ Deployment/friendly_comp_name.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
-
+#!/bin/bash
 
 # 20211004
 
-# Create friendly computer name from Computer distName
+# Create friendly computer name from Computer Name
+# Assumes format vh999mc99999mac99
 
 name=$(scutil --get ComputerName)
 asset_id=${name:5:7}
 room=${name:2:3}
 uc_asset_id=$(echo "$asset_id" | tr '[:lower:]' '[:upper:]')
-
 friendly_name="Victory-$room-${name:(-2)}"
+
 echo $name
 echo $friendly_name
 echo $uc_asset_id
+
+# Uncomment below to set ARD fields
 
 # /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -computerinfo -set1 -1 $uc_asset_id
 
