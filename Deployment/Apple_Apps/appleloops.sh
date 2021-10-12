79d9c7e1bd1710d0325b6fea774b215555e9de69
PJ White <pwhite@mercy.edu>
2021-10-12 15:39:50 -0400

    Add comments, remove second appleloops level

--- Deployment/Apple_Apps/appleloops.sh
+++ Deployment/Apple_Apps/appleloops.sh
@@ -2,9 +2,10 @@
 
 # Install or update Apple packages for Logic and Garageband (and MainStage if installed)
 
-# Assumes install and location of appleloops and managedpython https://github.com/macadmins/python
+# Assumes install and location of appleloops and managed_python https://github.com/macadmins/python
 # Assumes location of content on http server
 
-/usr/local/bin/managed_python3 /usr/local/bin/appleloops/appleloops --deployment --pkg-server http://munki6.digiarts.mercy:8090/appleloops_content --mandatory --optional
+# Use sudo if outside of this script
+/usr/local/bin/managed_python3 /usr/local/bin/appleloops --deployment --pkg-server http://munki6.digiarts.mercy:8090/appleloops_content --mandatory --optional
 
 exit 0
