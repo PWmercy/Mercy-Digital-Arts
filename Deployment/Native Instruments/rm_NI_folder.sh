a4449c7e21ba0de7c3a139a760695c828887b630
PJ White <pwhite@mercy.edu>
2022-02-28 10:18:14 -0500

    Minor

--- Deployment/Native Instruments/rm_NI_folder.sh
+++ Deployment/Native Instruments/rm_NI_folder.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Reaktor 6 and other products were showing as “Demo”, even though
-# they should be licensed as part with Komplete 12 Ultimate
+# they should be licensed as part of Komplete 12 Ultimate
 
 # To resolve, delete /Users/Shared/Native Instruments folder
 
 # This forces re-read of all licenses.
- 
+
 # See: Native Instruments Support article
 # https://support.native-instruments.com/hc/en-us/articles/360000848825
 
