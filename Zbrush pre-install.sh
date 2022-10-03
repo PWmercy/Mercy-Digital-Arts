1c5072593e40c5e823e6528e94965cc5bb7d5118
PJ White <pwhite@mercy.edu>
2022-10-03 15:20:42 -0400

    From Munki admin

---
+++ Zbrush pre-install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if  [[ -e "/Applications/ZBrush 2022 FL/Uninstall/Uninstall ZBrush 2022.app" ]]; then
+
+cd "/Applications/ZBrush 2022 FL/Uninstall/Uninstall ZBrush 2022.app/Contents/MacOS"
+
+##### CHECK CPU TYPE #####
+
+cpu_type=$(sysctl -a | grep brand)
+
+if [[ $cpu_type =~ ""Intel"" ]]; then
+
+     ./osx-x86_64 --mode unattended
+
+elif [[ $cpu_type =~ ""Apple"" ]]; then
+
+	./osx-arm64 --mode unattended
+
+fi
+
+fi
