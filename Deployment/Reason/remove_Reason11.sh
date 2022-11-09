d862d28f3828ee63430ede5f6fce859b16392f87
PJ White <pwhite@mercy.edu>
2022-11-09 13:21:17 -0500

    Tidy up

--- Deployment/Reason/remove_Reason11.sh
+++ Deployment/Reason/remove_Reason11.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 if [[ -e "/Applications/Reason 12.app" ]]
-
 then
 
 if [[ -e "/Applications/Reason 11.app" ]]
-
 then
-	echo "I'll delete it"
 	rm -r "/Applications/Reason 11.app"
+    echo "Reason 11 removed"
 
 else
 	echo "nothing here"
