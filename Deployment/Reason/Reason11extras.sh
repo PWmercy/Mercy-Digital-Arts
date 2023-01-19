4742ca9cf06cb39c5dc108b09e7889e78aa558c0
PJ White <pwhite@mercy.edu>
2023-01-19 12:19:19 -0500

    Minor edit to comments

--- Deployment/Reason/Reason11extras.sh
+++ Deployment/Reason/Reason11extras.sh
@@ -22,10 +22,11 @@ then
     fi
     # Then create symbolic link in local Music folder (to central Library folder)
     ln -s  "/Library/Application Support/Propellerhead Software/Propellerhead Content" "/Users/$USER/Music/Propellerhead Content"
-    # chown root:admin for /Library/Application Support folder
+    # chown root:admin for /Library/Application Support folder:
     # chown -R root:admin "/Library/Application Support/Propellerhead Software/Propellerhead Content"
     # chmod -R go+rx "/Library/Application Support/Propellerhead Software/Propellerhead Content"
 
 else echo "Content not found in main library."
 
 fi
+
