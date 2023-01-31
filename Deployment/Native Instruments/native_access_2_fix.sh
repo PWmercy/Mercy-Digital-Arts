76cc4a68045668f6a8eaad1f4e405c14ce24241e
PJ White <pwhite@mercy.edu>
2023-01-31 12:39:05 -0500

    Initial commit

---
+++ Deployment/Native Instruments/native_access_2_fix.sh
@@ -0,0 +1,14 @@
+# No guarantees. If necessary, downgrade to Native Access 1
+# https://support.native-instruments.com/hc/en-us/articles/4748946468497-How-to-Downgrade-Native-Access-2-to-Native-Access-1
+
+rm "Macintosh HD/Library/Launchdaemons/com.native-instruments.NativeAccess.Helper2.plist"
+
+rm "Macintosh HD/Library/Privilegedhelpertools/com.native-instruments.NativeAccess.Helper2"
+
+rm "Macintosh HD/Users/*Your User Name*/Library/Preferences/com.native-instruments.Native Instruments.plist"
+
+rm "Macintosh HD/Users/*Your User Name*/Library/Preferences/com.native-instruments.NTKDaemon.plist"
+
+rm "Macintosh HD/Users/*Your User Name*/Library/Application Support/Native Instruments NTK (folder)"
+
+rm "Macintosh HD/Users/*Your User Name*/Library/Application Support/Native Instruments/Native Access"
