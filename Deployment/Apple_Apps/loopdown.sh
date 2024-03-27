1e6c35dbff001de6ac1e786cf8da9010cbc8722d
PJ White <pwhite@mercy.edu>
2024-03-27 14:25:10 -0400

    Create loopdown.sh

---
+++ Deployment/Apple_Apps/loopdown.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Install or update Apple packages for Logic and Garageband (and MainStage if installed)
+
+
+# NOTE this originally used AppleLoops, which was superceded by loopdown (https://github.com/carlashley/loopdown)
+
+# Assumes install and location of loopdown and managed_python https://github.com/macadmins/python
+# Assumes location of content on http server
+
+## Use macadmin python
+## run /usr/local/bin/managed_python3 /usr/local/bin/loopdown/loopdown -h for details
+## -a = app, -i = install, -m = mandatory -o = optional
+
+/usr/local/bin/managed_python3 /usr/local/loopdown/loopdown -a garageband logicpro --i --pkg-server https://munki6.digiarts.mercy:8090/appleloops_content/loopdown --mandatory --optional
+################################################
+##### Use sudo if outside of this script #######
+################################################
+
+
+exit 0
