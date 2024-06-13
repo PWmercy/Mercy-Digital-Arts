7d7d0d8b1d4fb18259e0bc5e74c99ce130cf4f9a
PJ White <pwhite@mercy.edu>
2024-06-13 09:50:07 -0400

    Add comments

--- Deployment/Apple_Apps/loopdown.sh
+++ Deployment/Apple_Apps/loopdown.sh
@@ -6,16 +6,20 @@
 # NOTE this originally used AppleLoops, which was superceded by loopdown (https://github.com/carlashley/loopdown)
 
 # Assumes install and location of loopdown and managed_python https://github.com/macadmins/python
-# Assumes location of content on http server
+# Assumes location of content on http server. As of 05/2024, content is on miniserv6, using MDS to run Apache
 
 ## Use macadmin python
-## run /usr/local/bin/managed_python3 /usr/local/bin/loopdown/loopdown -h for details
+## run
+##
+## /usr/local/bin/managed_python3 /usr/local/bin/loopdown/loopdown -h
+##
+## for details
 ## -a = app, -i = install, -m = mandatory -o = optional
 
-/usr/local/bin/managed_python3 /usr/local/loopdown/loopdown -a garageband logicpro -i --pkg-server https://munki6.digiarts.mercy:8090/appleloops_content/loopdown --mandatory --optional
-################################################
+ ################################################
 ##### Use sudo if outside of this script #######
 ################################################
 
+/usr/local/bin/managed_python3 /usr/local/loopdown/loopdown -a garageband logicpro --i --pkg-server https://munki6.digiarts.mercy:8090/appleloops_content/loopdown --mandatory --optional
 
 exit 0
