fbaa285f628f3f0e94e8b6f79e949044dcaa244c
PJ White <pwhite@mercy.edu>
2024-12-12 16:17:10 -0500

    add more commands

--- Deployment/Set_Network_locations.sh
+++ Deployment/Set_Network_locations.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 
-# Edited 20210113
+# Edited 202412
 
 # Commands to set and get Network settings and locations
 # Also see https://gist.github.com/jjnilton/add1eeeb3a9616f53e4c
 
-networksetup -createlocation "Digiarts DNS" populate
-networksetup -switchtolocation "Digiarts DNS"
-
-# setdnsservers parameter (usually "Ethernet") may vary depending on interfaces
-# To get interfaces, use
-# networksetup -listallnetworkservices
-
-networksetup -setdnsservers "Ethernet" 172.31.48.124
 networksetup -listlocations
 networksetup -getcurrentlocation
 
+
+# Get list of nameservers for current location:
+# scutil --dns
+
+networksetup -createlocation "Digiarts DNS" populate
+networksetup -switchtolocation "Digiarts DNS"
+networksetup -setdnsservers "Ethernet" 172.31.48.124
+
+# Get list of nameservers for current location:
+scutil --dns
+
+# setdnsservers parameter (usually "Ethernet") may vary depending on interfaces
+# To get interfaces, use
+networksetup -listallnetworkservices
+
+networksetup -listlocations
+networksetup -getcurrentlocation
+
+# To empty current location:
+sudo networksetup -setdnsservers "Ethernet" "Empty"
+
 # Create backup location - point to Main Synology
+
 networksetup -createlocation "Digiarts DNS2" populate
 networksetup -switchtolocation "Digiarts DNS2"
 networksetup -setdnsservers "Ethernet" 172.31.48.200
-
 
 # To clear DNS setting in Automatic to get to default
 networksetup -switchtolocation "Automatic"
